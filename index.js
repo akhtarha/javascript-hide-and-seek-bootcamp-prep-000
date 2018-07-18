ac20911536c77091c21e48421bb09b003799e248
Hassan A <has_is_ere@hotmail.com>
2018-07-18 13:49:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function deepestChild(){
 }
 
 function increaseRankBy(n){
-  const rankedList = document.querySelectorAll('ul.ranked-list')
+  const rankedList = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   
   for (let i=0; i < ranked-list.length; i++) {
     rankedList[i].innerHTML = parseInt(rankedList[i].innerHTML + n)
